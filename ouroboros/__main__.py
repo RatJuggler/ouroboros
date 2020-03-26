6a1d0943549a498abf5eabfffe30c251ac87f456
John Chase <ratteal@gmail.com>
2020-03-26 21:53:09 +0000

    Add some primative scoring.

--- ouroboros/__main__.py
+++ ouroboros/__main__.py
@@ -54,8 +54,8 @@ class Snake(pygame.sprite.Sprite):
             self.rectangle.left = 0
         if self.rectangle.right > DISPLAY_WIDTH:
             self.rectangle.right = DISPLAY_WIDTH
-        if self.rectangle.top < 0:
-            self.rectangle.top = 0
+        if self.rectangle.top < CELL_SIZE:
+            self.rectangle.top = CELL_SIZE
         if self.rectangle.bottom > DISPLAY_HEIGHT:
             self.rectangle.bottom = DISPLAY_HEIGHT
 
@@ -67,7 +67,7 @@ class Food(pygame.sprite.Sprite):
         self.surface = pygame.Surface((CELL_SIZE, CELL_SIZE))
         self.surface.fill((255, 32, 0))
         self.rectangle = self.surface.get_rect(
-            topleft=(random.randint(0, CELL_WIDTH - 1) * CELL_SIZE, random.randint(0, CELL_HEIGHT - 1) * CELL_SIZE)
+            topleft=(random.randint(0, CELL_WIDTH - 1) * CELL_SIZE, random.randint(1, CELL_HEIGHT - 1) * CELL_SIZE)
         )
 
 
@@ -91,6 +91,12 @@ def draw_background(screen: pygame.Surface) -> None:
         pygame.draw.line(screen, GRID_COLOUR, (grid_column, 0), (grid_column, DISPLAY_HEIGHT))
 
 
+def display_score(screen: pygame.Surface, score: int) -> None:
+    font = pygame.font.SysFont(None, 24)
+    score_img = font.render(str(score), True, (255, 255, 255))
+    screen.blit(score_img, ((CELL_WIDTH - 4) * CELL_SIZE, 2))
+
+
 def play() -> None:
     screen = pygame.display.set_mode((DISPLAY_WIDTH, DISPLAY_HEIGHT))
     pygame.display.set_caption('Ouroboros')
@@ -98,6 +104,7 @@ def play() -> None:
     snake = Snake()
     food = Food()
     direction = MOVE_RIGHT
+    score = 0
     while True:
         for event in pygame.event.get():
             if event.type == QUIT:
@@ -108,8 +115,10 @@ def play() -> None:
         direction = decode_input(direction, pygame.key.get_pressed())
         snake.move(direction)
         if snake.rectangle.topleft == food.rectangle.topleft:
+            score += 1
             food = Food()
         draw_background(screen)
+        display_score(screen, score)
         screen.blit(snake.surface, snake.rectangle)
         screen.blit(food.surface, food.rectangle)
         pygame.display.flip()
