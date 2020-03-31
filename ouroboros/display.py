6c7f3ed5b6052584a814bdb0f94ced4b55ff0321
John Chase <ratteal@gmail.com>
2020-03-31 17:06:48 +0100

    Move to image sprites instead of colour blocks.

--- ouroboros/display.py
+++ ouroboros/display.py
@@ -22,9 +22,9 @@ class Display:
         self._screen = pygame.display.set_mode((self.DISPLAY_WIDTH, self.DISPLAY_HEIGHT))
         pygame.display.set_caption('Ouroboros')
 
-    def create_surface(self, colour: RGB) -> pygame.Surface:
+    def create_surface(self, image_sheet, image_start) -> pygame.Surface:
         surface = pygame.Surface((self.CELL_SIZE, self.CELL_SIZE))
-        surface.fill(colour)
+        surface.blit(image_sheet, (0, 0), (image_start[0], image_start[1], self.CELL_SIZE, self.CELL_SIZE))
         return surface
 
     def get_rect(self, at_cell: Tuple[int, int]):
--- ouroboros/sprite_images.py
+++ ouroboros/sprite_images.py
@@ -14,14 +14,15 @@ class SpriteImages:
 
     @classmethod
     def load_images(cls, display: Display) -> 'SpriteImages':
+        image_sheet = pygame.image.load('oroborus.png').convert()
         images = {}
         with open('sprite_images.json') as json_file:
             sprite_images = json.load(json_file)
             for for_class, direction_images in sprite_images.items():
                 class_images = {}
                 image_details = sprite_images[for_class]
-                for direction, colour in image_details.items():
-                    class_images[direction] = display.create_surface(tuple(map(int, colour[1:-1].split(', '))))
+                for direction, image_start in image_details.items():
+                    class_images[direction] = display.create_surface(image_sheet, tuple(map(int, image_start.split(', '))))
                 images[for_class] = class_images
         return SpriteImages(display, images)
 
