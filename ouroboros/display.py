e74df3f438f9625982ee918387f0766af6dbd6ed
John Chase <ratteal@gmail.com>
2020-04-03 17:26:31 +0100

    Always show the line separating score from playing area.

--- ouroboros/display.py
+++ ouroboros/display.py
@@ -60,8 +60,9 @@ class Display:
 
     def draw_background(self, draw_grid: bool = True, score: int = 0) -> None:
         self._screen.fill(BACKGROUND_COLOUR)
+        pygame.draw.line(self._screen, TEXT_COLOUR, (0, self.CELL_SIZE), (self.DISPLAY_WIDTH, self.CELL_SIZE))
         if draw_grid:
-            for grid_row in range(self.CELL_SIZE, self.DISPLAY_HEIGHT, self.CELL_SIZE):
+            for grid_row in range(self.CELL_SIZE * 2, self.DISPLAY_HEIGHT, self.CELL_SIZE):
                 pygame.draw.line(self._screen, GRID_COLOUR, (0, grid_row), (self.DISPLAY_WIDTH, grid_row))
             for grid_column in range(0, self.DISPLAY_WIDTH, self.CELL_SIZE):
                 pygame.draw.line(self._screen, GRID_COLOUR, (grid_column, self.CELL_SIZE), (grid_column, self.DISPLAY_HEIGHT))
